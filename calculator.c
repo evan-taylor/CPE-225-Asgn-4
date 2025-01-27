4be32e2c08d924ad33abbafe6fb83f28e27324c4
isaacericsson222 <isaac.h.ericsson@gmail.com>
2025-01-27 15:33:00 -0800

    Calc started

---
+++ calculator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+int main() {
+    int opNum;
+    int num1;
+    int num2;
+    int operation;
+    char yn;
+
+
+    opNum = 0;
+    printf("Welcome to the Calculator program.\n");
+    printf("Operations - 1:add 2:subtract 3:multiply 4:divide 5:and 6:or 7:xor 8:lshift 9:rshift\n");
+
+
+    do {
+        printf("Number of operations performed: %d\n", opNum);
+        printf("Enter number: ");
+        scanf(num1);
+        printf("\n");
+        printf("Enter second number");
+        opNum ++;
+        printf("Continue (y/n)?: ");
+        scanf(" %c", &yn);
+    } while(yn != 'n');
+    
+    printf("Exiting");
+    return 0;
+
+}
