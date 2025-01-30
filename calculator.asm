4723c596eb9a568ff51650c72f26d880686a75e8
Evan Taylor <eltaylor1104@gmail.com>
2025-01-29 17:53:51 -0800

    Established data

--- calculator.asm
+++ calculator.asm
@@ -1,24 +1,19 @@
 .globl main
+
 .text
-
 main:
-	li a0, 2
-	jal printint
-	
-	la a0, msg1
-	jal printstring
-	
-	li a0, 'A'
-	jal printchar
-	
-	jal readchar
-	
-	jal readint
-	
-	jal exit0
-	
+    
 
-	
 .data   
-    msg1: .string "Test"
-
+    welcome_msg:    .string "Welcome to the Calculator program.\n"
+    operations_msg: .string "Operations - 1:add 2:subtract 3:multiply 4:divide 5:and 6:or 7:xor 8:lshift 9:rshift"
+    op_count_msg:   .string "\n\nNumber of operations performed: "
+    enter_num1_msg: .string "\nEnter number: "
+    enter_num2_msg: .string "Enter second number: "
+    select_op_msg:  .string "Select operation: "
+    result_msg:     .string "Result: "
+    continue_msg:   .string "\nContinue (y/n)?: "
+    exit_msg:       .string "\nNumber of operations performed: "
+    exiting_msg:    .string "Exiting\n"
+    div_zero_msg:   .string "Error: Division by zero!\n"
+    invalid_op_msg: .string "Invalid Operation\n"
